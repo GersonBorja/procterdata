71e9f77bed6abd2f980a4b067dd3c12d5be4ae11
Gerson Borja <gerson.borja2018@gmail.com>
2023-03-20 13:28:12 -0600

    Se termino la clase Cintillo

--- api/clases/Cintillo.php
+++ api/clases/Cintillo.php
@@ -17,15 +17,15 @@ class Cintillo extends Database {
         $this->autor = $autor;
     }
     
-  public function crearCintillo() {
+  public function agregarCintillo() {
     if(empty($this->cantidad) || empty($this->precio)){
-          $this->response["msg"] = "Debes completar todos los campos";
+          $this->response['msg'] = 'Debes completar todos los campos';
           return $this->response;
     }else if(!is_numeric($this->cantidad)){
-          $this->response["msg"] = "La cantidad de cintillos debe ser en numeros";
+          $this->response['msg'] = 'La cantidad de cintillos debe ser en numeros';
           return $this->response;
     }else if(!is_numeric($this->precio)){
-          $this->response["msg"] = "El campo precio solo admite numeros";
+          $this->response['msg'] = 'El campo precio solo admite numeros';
           return $this->response;
     }else{
       if($this->cantidad > 1){
@@ -34,15 +34,36 @@ class Cintillo extends Database {
                   $this->ejecutarConsulta($sql, [$this->interno, $this->barra, $this->descripcion, $this->cantidad, $this->precio, $this->autor]);
             }
             $this->response['status'] = 'OK';
-            $this->response["msg"] = 'Se han añadido ' . $this->cantidad . ' a la lista';
+            $this->response['msg'] = 'Se han añadido ' . $this->cantidad . ' cintillos a la lista';
             return $this->response;
       }else{
             $sql = 'INSERT INTO codigos (interno, barra, descripcion, cantidad, precio, autor) VALUES (?, ?, ?, ?, ?, ?)';
             $this->ejecutarConsulta($sql, [$this->interno, $this->barra, $this->descripcion, $this->cantidad, $this->precio, $this->autor]);
             $this->response['status'] = 'OK';
-            $this->response["msg"] = 'Se ha añadido ' . $this->cantidad . ' a la lista';
+            $this->response["msg"] = 'Se ha añadido ' . $this->cantidad . ' cintillos a la lista';
             return $this->response;
       }
     }
+  }
+  
+  public function editarCintillo($interno, $items = ['descripcion' => '', 'precio' => '']){
+    $sql = 'UPDATE codigos SET descripcion = ?, precio = ? WHERE interno = ?';
+    $editar = $this->ejecutarConsulta($sql, [$items['descripcion'], $items['precio'], $interno]);
+    $cantidad = $editar->rowCount();
+    $this->response['status'] = 'OK';
+    $this->response["msg"] = $cantidad . ' cintillos actualizados';
+    return $this->response;
+  }
+  
+  public function eliminarCintillos($autor){
+    $sql = 'DELETE FROM codigos WHERE autor = ?';
+    $this->ejecutarConsulta($sql, [$autor]);
+    $this->response['status'] = 'OK';
+    $this->response["msg"] = 'Listado de cintillos eliminados';
+    return $this->response;
+  }
+  
+  public function generarDocumentoCintillos($autor){
+    
   }
 }
--- api/clases/Notificacion.php
+++ api/clases/Notificacion.php
@@ -9,8 +9,8 @@ class Notificacion {
     public function crearNotificacion($titulo, $cuerpo, $imagen='https://procter.work/public/img/logo/p&g.svg') {
 
         $beamsClient = new PushNotifications(array(
-            "instanceId" => "60d87988-4708-453e-aab5-0b55703a8b2f",
-            "secretKey" => "B0D22953E9CB7B6C4693BC73425F1E64C4740688809486214110BFC20C591503",
+            "instanceId" => "",
+            "secretKey" => "",
         ));
 
         $data = array(
